afbfdaa02b34b957401b3ce6edc89fc0da492ac8
Shounak Lohokare <shounaklohokare@gmail.com>
2022-11-25 00:23:49 +0530

    created loop animation for tunes

---
+++ app.js
@@ -0,0 +1,41 @@
+class DrumKit{
+    constructor(){
+        this.pads = document.querySelectorAll('.pad');
+        this.playBtn = document.querySelector('.play');
+        this.kickAudio = document.querySelector('.kick-sound');
+        this.snareAudio = document.querySelector('.snare-sound');
+        this.hihatAudio = document.querySelector('.hihat-sound');
+        this.index = 0;
+        this.bpm = 150;
+    }
+
+    activePad(){
+        this.classList.toggle('active');
+    }
+
+    repeat(){
+        let step = (this.index) % 8;
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        this.index++;
+    }
+
+    start(){
+        const interval = (60/this.bpm) * 1000;
+        setInterval(() => {
+            this.repeat();
+        }, 1000);
+    }
+}
+
+const drumKit = new DrumKit();
+
+// drumKit.start();
+
+drumKit.pads.forEach(pad => {
+    pad.addEventListener('click', drumKit.activePad);
+})
+
+drumKit.playBtn.addEventListener('click', () =>{
+    drumKit.start();
+});
+
