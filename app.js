36435eca6ed4b6a56f858d7c5a33875142756871
Shounak Lohokare <shounaklohokare@gmail.com>
2022-12-01 15:20:33 +0530

    added an ability to change sounds

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ class DrumKit{
     constructor(){
         this.pads = document.querySelectorAll('.pad');
         this.playBtn = document.querySelector('.play');
+        this.currentKick = './sounds/kick-classic.wav';
+        this.currentSnare = './sounds/snare-acoustic01.wav';
+        this.currentHihat = './sounds/hihat-acoustic01.wav';
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
+        this.selects = document.querySelectorAll('select');
     }
 
     activePad(){
@@ -61,6 +65,29 @@ class DrumKit{
             this.playBtn.classList.remove('active');
         }
     }
+
+    changeSound(e){
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+        console.log(selectionName);
+
+        switch(selectionName){
+
+            case "kick-select":
+                this.kickAudio.src = selectionValue;
+                break;
+
+            case "snare-select":
+                this.snareAudio.src = selectionValue;
+                break;
+
+            case "hihat-select":
+                this.hihatAudio.src = selectionValue;
+                break;
+
+
+        }
+    }
 }
 
 const drumKit = new DrumKit();
@@ -72,7 +99,7 @@ drumKit.pads.forEach(pad => {
     pad.addEventListener("animationend", function(){
         this.style.animation = "";
     });
-})
+});
 
 drumKit.playBtn.addEventListener('click', () =>{
     //this.playBtn.classList.toggle('play-stop');
@@ -80,3 +107,10 @@ drumKit.playBtn.addEventListener('click', () =>{
     drumKit.start();
 });
 
+drumKit.selects.forEach(select =>{
+
+    select.addEventListener('change', function(e){
+        drumKit.changeSound(e);
+    });
+
+});
